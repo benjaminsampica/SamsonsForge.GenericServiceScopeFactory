48c28aa304d0e93b4c2a72c52c92053504bfd1d4
Ben Sampica <benjamin.sampica@gmail.com>
2021-06-07 22:21:32 -0500

    update xml and secret key

--- SamsonsForge.TypedServiceScopeFactory/DependencyInjectionExtensions.cs
+++ SamsonsForge.TypedServiceScopeFactory/DependencyInjectionExtensions.cs
@@ -2,8 +2,15 @@
 
 namespace SamsonsForge.TypedServiceScopeFactory
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public static class DependencyInjectionExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     {
+        /// <summary>
+        ///     Adds the <see cref="IServiceScopeFactory{T}"/> as a singleton to the service container.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <returns></returns>
         public static IServiceCollection AddTypedServiceScopeFactory(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton(typeof(IServiceScopeFactory<>), typeof(ServiceScopeFactory<>));
