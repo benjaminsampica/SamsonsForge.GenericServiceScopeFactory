538511475574cd6981b9be695ed5e6df69a97615
Ben Sampica <benjamin.sampica@gmail.com>
2021-06-07 22:17:40 -0500

    Update xml docs

--- SamsonsForge.TypedServiceScopeFactory/ServiceScopeFactory.cs
+++ SamsonsForge.TypedServiceScopeFactory/ServiceScopeFactory.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 
 namespace SamsonsForge.TypedServiceScopeFactory
 {
+    /// <inheritdoc cref="IServiceScopeFactory" />
     public interface IServiceScopeFactory<out T> where T : class
     {
         /// <inheritdoc cref="IServiceScope" />
         IServiceScope<T> CreateScope();
     }
 
+    /// <inheritdoc cref="IServiceScope" />
     public interface IServiceScope<out T> : IDisposable where T : class
     {
         /// <inheritdoc cref="ServiceProviderServiceExtensions.GetRequiredService{T}(IServiceProvider)" />
@@ -20,19 +22,24 @@ namespace SamsonsForge.TypedServiceScopeFactory
         IEnumerable<T> GetServices();
     }
 
+    /// <inheritdoc cref="IServiceScopeFactory" />
     public class ServiceScopeFactory<T> : IServiceScopeFactory<T> where T : class
     {
         private readonly IServiceScopeFactory _serviceScopeFactory;
 
+        /// <inheritdoc cref="IServiceScopeFactory" />
         public ServiceScopeFactory(IServiceScopeFactory serviceScopeFactory) => _serviceScopeFactory = serviceScopeFactory;
 
+        /// <inheritdoc cref="ServiceProviderServiceExtensions.CreateScope(IServiceProvider)" />
         public IServiceScope<T> CreateScope() => new ServiceScope<T>(_serviceScopeFactory.CreateScope());
     }
 
+    /// <inheritdoc cref="IServiceScope" />
     public class ServiceScope<T> : IServiceScope<T> where T : class
     {
         private readonly IServiceScope _scope;
 
+        /// <inheritdoc cref="IServiceScopeFactory.CreateScope" />
         public ServiceScope(IServiceScope scope) => _scope = scope;
 
         /// <inheritdoc />
@@ -46,13 +53,16 @@ namespace SamsonsForge.TypedServiceScopeFactory
 
         private bool _disposed = false;
 
+        /// <inheritdoc cref="IDisposable.Dispose" />
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         protected virtual void Dispose(bool calledFromCodeNotTheGarbageCollector)
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             if (_disposed)
             {
@@ -67,7 +77,9 @@ namespace SamsonsForge.TypedServiceScopeFactory
             _disposed = true;
         }
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         ~ServiceScope()
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             Dispose(false);
         }
